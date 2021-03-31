0fde24e34ac23c8d5c8d442c893fc6a0837e4dab
Sarika Mittra <sarikamittra@gmail.com>
2021-03-31 12:00:13 -0500

    added multiplication and comments

---
+++ math.py
@@ -0,0 +1,22 @@
+# add.py 
+# by Matt
+
+def sum(a, b):
+	"""generates the sum of two numbers"""
+	c = a + b
+	return c
+
+def prod(a, b):
+	"""generates the product of two numbers"""
+	c = a * b
+	return c
+
+print()
+ans = sum(3, 4)
+print('sum = ', ans)
+print()
+ans = prod(5, 7)
+print('product = ', ans)
+print()
+
+# end add.py
